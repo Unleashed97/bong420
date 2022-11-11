d707a54fcdbd5308ed14e1e475ef801d1a281f6f
Nichkov Roman <nichkovroman@gmail.com>
2022-11-11 11:35:43 +0300

    feat: add section new

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,7 @@ import * as customFunctions from './modules/functions.js'
 // vendors
 
 // swiper
-const swiper = new Swiper('.swiper--sale', {
+const swiperSale = new Swiper('.swiper--sale', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
@@ -20,7 +20,29 @@ const swiper = new Swiper('.swiper--sale', {
 
     // Navigation arrows
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next--sale',
+        prevEl: '.swiper-button-prev--sale',
+    },
+})
+
+const swiperNew = new Swiper('.swiper--new', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 20,
+
+    breakpoints: {
+        // when window width is >= 320px
+        1440: {
+            slidesPerView: 5,
+            spaceBetween: 20,
+        },
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next--new',
+        prevEl: '.swiper-button-prev--new',
     },
 })
