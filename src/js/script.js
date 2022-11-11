1fa0c933caca1bb590c8f0badc3002c08718bb8a
Nichkov Roman <nichkovroman@gmail.com>
2022-11-11 09:57:13 +0300

    feat: add section sale

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,26 @@
 import * as customFunctions from './modules/functions.js'
 
 // vendors
+
+// swiper
+const swiper = new Swiper('.swiper--sale', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 50,
+
+    breakpoints: {
+        // when window width is >= 320px
+        1440: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+        },
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+})
